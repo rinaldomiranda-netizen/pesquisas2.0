74ffc5adb6db608408fbd8145fc8e851c3c9aa85
rinaldomiranda-netizen <rinaldomiranda@hotmail.com>
2026-09-18 12:47:08 -0300

    Remover redirecionamento administrativo fixo e separar cache por acesso

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE='pesquisa20-v3';
-const ADMIN_TOKEN='PESQUISA20-ADMIN-RINALDO-2026';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
+const CACHE='pesquisa20-v5';
+const CORE=['./','./index.html','./manifest.webmanifest','./admin.html','./diretoria.html','./pesquisador.html','./manifest-admin.webmanifest','./manifest-diretoria.webmanifest','./manifest-pesquisador.webmanifest','./icon-192.svg','./icon-512.svg','./icon-admin-192.svg','./icon-admin-512.svg','./icon-diretoria-192.svg','./icon-diretoria-512.svg','./icon-pesquisador-192.svg','./icon-pesquisador-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const u=new URL(event.request.url);if(u.origin===self.location.origin&&u.pathname.endsWith('/pesquisas2.0/')){u.search='?access='+encodeURIComponent(ADMIN_TOKEN);event.respondWith(Response.redirect(u.href,302));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));});
