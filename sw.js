605f6203150cd812b7e430d9e0a40c4b95c8836e
rinaldomiranda-netizen <rinaldomiranda@hotmail.com>
2026-09-18 10:09:28 -0300

    Fixar entrada administrativa do Pesquisa 2.0 e atualizar cache PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='pesquisa20-v2';
+const CACHE='pesquisa20-v3';
+const ADMIN_TOKEN='PESQUISA20-ADMIN-RINALDO-2026';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const u=new URL(event.request.url);if(u.origin===self.location.origin&&u.pathname.endsWith('/pesquisas2.0/')){u.search='?access='+encodeURIComponent(ADMIN_TOKEN);event.respondWith(Response.redirect(u.href,302));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));});
