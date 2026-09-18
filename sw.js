4e2b64a7a5e996c529fe5cc99fb932b6a862dec0
rinaldomiranda-netizen <rinaldomiranda@hotmail.com>
2026-09-18 18:48:11 -0300

    Atualizar cache para correção de sessão por perfil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pesquisa20-v25';
+const CACHE='pesquisa20-v26';
 const CORE=['./','./index.html','./fixes.js','./manifest.webmanifest','./admin.html','./diretoria.html','./pesquisador.html','./manifest-admin.webmanifest','./manifest-diretoria.webmanifest','./manifest-pesquisador.webmanifest','./icon-192.svg','./icon-512.svg','./icon-admin-192.svg','./icon-admin-512.svg','./icon-diretoria-192.svg','./icon-diretoria-512.svg','./icon-pesquisador-192.svg','./icon-pesquisador-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
